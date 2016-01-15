7862a6f3a2300b4765068769f7aeb04dfbea8b73
sumi12345 <753007687@qq.com>
2016-01-15 02:51:23 +0000

    gulp-wrap

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var prefix = require('gulp-autoprefixer');
+var wrap = require('gulp-wrap');
 
 gulp.task('sass', function(){
     gulp.src('styles/main.scss')
@@ -9,14 +10,15 @@ gulp.task('sass', function(){
         .pipe(gulp.dest('../styles'));
 });
 
-gulp.task('copy', function() {
-    gulp.src('index.html')
-        .pipe(gulp.dest('..'))
+gulp.task('build', function() {
+    gulp.src("pages/*.html")
+        .pipe(wrap({src:"layout/default.html"}))
+        .pipe(gulp.dest(".."));
 });
 
 gulp.task('watch', function() {
-    gulp.watch(['*.html'], ['copy']);
+    gulp.watch(['pages/*.html', 'layout/*.html'], ['build']);
     gulp.watch(['styles/*.scss'], ['sass']);
 });
 
-gulp.task('default', ['sass', 'copy', 'watch']);
+gulp.task('default', ['sass', 'build', 'watch']);
